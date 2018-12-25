e20f3927da0bf63624f75d9464abe67cf0c1fa52
VictoremWinbringer <victor@mail.ru>
2018-12-26 01:54:14 +0300

    change ClientWithId

--- src/business_logic_layer.rs
+++ src/business_logic_layer.rs
@@ -5,10 +5,6 @@ use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-lazy_static! {
-static ref IDS: Mutex<HashMap<String,u64>> = Mutex::new(HashMap::new());
-}
-
 struct Client {
     socket: TypedClientSocket
 }
@@ -29,31 +25,24 @@ impl Client {
 }
 
 struct ClientWithId {
-    key: String,
+    id:u64,
     client: Client,
 }
 
 impl ClientWithId {
     fn new(port: &str, server_address: &str) -> Result<ClientWithId, Box<dyn Error>> {
-        let client = Client::new(port, server_address)?;
-        let key = port.to_owned() + ":" + server_address;
-        IDS.lock()?
-            .entry(key)
-            .or_insert(1);
-        Ok(ClientWithId { key, client })
+        let client = Client::new(port, server_address)?;;
+        Ok(ClientWithId { id:1, client })
     }
 
     fn send(&mut self, command: Vec<u8>) -> Result<usize, Box<dyn Error>> {
-        let mut ids = IDS.lock()?;
-        let old_id = ids[&self.key];
-        let count = self.client.send(old_id, command)?;
-        ids.entry(self.key)
-            .and_modify(|v| v += 1);
-        Ok(count)
+        let id = self.id.clone();
+        self.id +=1;
+       self.client.send(id, command)
     }
 
     fn recv(&mut self) -> Result<StatePacket, Box<dyn Error>> {
-        self.socket.read()
+        self.client.recv()
     }
 }
 
